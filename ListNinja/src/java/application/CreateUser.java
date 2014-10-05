44b0595f7ff4d03c9abfd72af048b305bf27c6d0
Gianna Fusaro <giannafusaro389@gmail.com>
2014-10-04 20:49:09 -0700

    delete user class

--- ListNinja/src/java/application/CreateUser.java
+++ ListNinja/src/java/application/CreateUser.java
@@ -12,7 +12,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import application.User;
+
+
 /**
  *
  * @author giannafusaro
