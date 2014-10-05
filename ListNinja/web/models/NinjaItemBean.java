9e9c667ac4e67c236d9df7dfed97ca623a440f5c
Robert Buser <supplyguyusmc@yahoo.com>
2014-10-04 18:23:52 -0700

    java beans for list,item,&ninja updated and sql set up in DB folder

--- ListNinja/web/models/NinjaItemBean.java
+++ ListNinja/web/models/NinjaItemBean.java
@@ -1,32 +1,32 @@
-
-import java.util.ArrayList;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package models;
+
+import java.beans.*;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  *
  * @author Buser
  */
-public class NinjaItem {
+public class NinjaItemBean implements Serializable {
+    
+    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
+    
+    private String sampleProperty;
+    
+    private PropertyChangeSupport propertySupport;
     
     //NinjaItem Variables
     private String itemName;
     private double itemPrice;
     ArrayList<String> ownerList = new ArrayList<String>();
     
-    /**Constructs a NinjaList
-     * 
-    */
-   public NinjaItem(String itemName)
-   {   
-	  this.itemName = itemName;
-   }
-   
-   /**Sets item price
+    /**Sets item price
      * 
     */
    public void setPrice(double  itemPrice){
@@ -68,5 +68,27 @@ public class NinjaItem {
    public int numOfOwners(){
        return ownerList.size(); 
     }
-   
+    
+    public NinjaItemBean() {
+        propertySupport = new PropertyChangeSupport(this);
+    }
+    
+    public String getSampleProperty() {
+        return sampleProperty;
+    }
+    
+    public void setSampleProperty(String value) {
+        String oldValue = sampleProperty;
+        sampleProperty = value;
+        propertySupport.firePropertyChange(PROP_SAMPLE_PROPERTY, oldValue, sampleProperty);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(listener);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(listener);
+    }
+    
 }
